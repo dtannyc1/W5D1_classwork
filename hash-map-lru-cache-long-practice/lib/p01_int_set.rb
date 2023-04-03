976841552e42052bcd4539aa83b926d68fd4c82f
dtannyc1 <dtannyc1@gmail.com>
2023-04-03 11:45:36 -0400

    Finished ResizingIntSet

--- hash-map-lru-cache-long-practice/lib/p01_int_set.rb
+++ hash-map-lru-cache-long-practice/lib/p01_int_set.rb
@@ -75,12 +75,28 @@ class ResizingIntSet
   end
 
   def insert(num)
+
+    if !include?(num)
+        self[num] << num
+        @count += 1
+        if @count > self.num_buckets
+            self.resize!
+        end
+        return true
+    else
+        return false
+    end
   end
 
   def remove(num)
+    if include?(num)
+        self[num].delete(num)
+        @count -= 1
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
@@ -90,9 +106,17 @@ class ResizingIntSet
   end
 
   def resize!
+    new_store = Array.new(num_buckets*2) { Array.new }
+    @store.each do |arr|
+        arr.each do |ele|
+            new_store[ele % (num_buckets*2)] << ele
+        end
+    end
+    @store = new_store
   end
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % self.num_buckets]
   end
 end

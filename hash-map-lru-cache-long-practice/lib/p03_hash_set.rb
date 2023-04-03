06b7bfb28ef470073df9b62e8f4df0dbe3f87479
dtannyc1 <dtannyc1@gmail.com>
2023-04-03 12:07:34 -0400

    Finished p03 HashSet Class

--- hash-map-lru-cache-long-practice/lib/p03_hash_set.rb
+++ hash-map-lru-cache-long-practice/lib/p03_hash_set.rb
@@ -7,12 +7,27 @@ class HashSet
   end
 
   def insert(key)
+    if !include?(key)
+        self[key] << key
+        @count += 1
+        if @count > self.num_buckets
+            self.resize!
+        end
+        return true
+    else
+        return false
+    end
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    if include?(key)
+        self[key].delete(key)
+        @count -= 1
+    end
   end
 
   private
@@ -22,9 +37,17 @@ class HashSet
   end
 
   def resize!
+    new_store = Array.new(num_buckets*2) { Array.new }
+    @store.each do |arr|
+        arr.each do |ele|
+            new_store[ele.hash % (num_buckets*2)] << ele
+        end
+    end
+    @store = new_store
   end
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
   end
 end

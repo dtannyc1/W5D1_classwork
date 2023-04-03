b8775b010ec3f8d666455a254611f964e7fdcc8a
dtannyc1 <dtannyc1@gmail.com>
2023-04-03 14:21:25 -0400

    Wrote HashMap Class

--- hash-map-lru-cache-long-practice/lib/p05_hash_map.rb
+++ hash-map-lru-cache-long-practice/lib/p05_hash_map.rb
@@ -1,49 +1,73 @@
 require_relative 'p04_linked_list'
 
 class HashMap
-  attr_accessor :count
+    include Enumerable
+    attr_accessor :count
 
-  def initialize(num_buckets = 8)
-    @store = Array.new(num_buckets) { LinkedList.new }
-    @count = 0
-  end
+    def initialize(num_buckets = 8)
+        @store = Array.new(num_buckets) { LinkedList.new }
+        @count = 0
+    end
 
-  def include?(key)
-  end
+    def include?(key)
+        @store[bucket(key)].include?(key)
+    end
 
-  def set(key, val)
-  end
+    def set(key, val)
+        if !@store[bucket(key)].update(key, val)
+            @store[bucket(key)].append(key, val)
+            @count += 1
+            if @count > self.num_buckets
+                self.resize!
+            end
+        end
+    end
 
-  def get(key)
-  end
+    def get(key)
+        @store[bucket(key)].get(key)
+    end
 
-  def delete(key)
-  end
+    def delete(key)
+        if @store[bucket(key)].remove(key)
+            @count -= 1
+        end
+    end
 
-  def each
-  end
+    def each(&prc)
+        @store.each do |linked_list|
+            linked_list.each do |node|
+                prc.call(node.key, node.val)
+            end
+        end
+    end
 
-  # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
-  
-  alias_method :[], :get
-  alias_method :[]=, :set
+    # uncomment when you have Enumerable included
+    def to_s
+      pairs = inject([]) do |strs, (k, v)|
+        strs << "#{k.to_s} => #{v.to_s}"
+      end
+      "{\n" + pairs.join(",\n") + "\n}"
+    end
 
-  private
+    alias_method :[], :get
+    alias_method :[]=, :set
 
-  def num_buckets
-    @store.length
-  end
+    private
 
-  def resize!
-  end
+    def num_buckets
+        @store.length
+    end
 
-  def bucket(key)
-    # optional but useful; return the bucket corresponding to `key`
-  end
+    def resize!
+        new_store = Array.new(num_buckets*2) { LinkedList.new }
+        self.each do |k, v|
+            new_store[k.hash % (num_buckets*2)].append(k,v)
+        end
+        @store = new_store
+    end
+
+    def bucket(key)
+        # optional but useful; return the bucket corresponding to `key`
+        key.hash % num_buckets
+    end
 end
